c6bfad25c9877d3c1df6323d3838ab86cebcb3b2
nao20010128nao <nao20010128@gmail.com>
2015-03-02 16:18:23 +0900

    Initial commit

---
+++ src/com/nao20010128nao/MCPE/SC/MainActivity.java
@@ -0,0 +1,17 @@
+package com.nao20010128nao.MCPE.SC;
+
+import android.app.*;
+import android.os.*;
+import android.view.*;
+import android.widget.*;
+
+public class MainActivity extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+	{
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
