bc2cc9ed91d4a2fb934819e902eadf5a6515cf8b
nao20010128@gmail.com <nao20010128@gmail.com>
2015-08-20 00:11:52 +0900

    ImagePref変更

--- src/pref/ImagePref.java
+++ src/pref/ImagePref.java
@@ -2,16 +2,20 @@ package pref;
 import android.util.*;
 import android.content.*;
 import android.view.*;
+import com.nao20010128nao.MC_PE.SkinChanger.*;
 
 public class ImagePref extends StartPref
 {
 	public ImagePref(Context c,AttributeSet attrs){
 		super(c,attrs);
+		setLayoutResource(R.layout.imagepref);
 	}
 
 	@Override
 	public View getView(View convertView, ViewGroup parent) {
 		// TODO: Implement this method
-		return super.getView(convertView, parent);
+		return ((LayoutInflater)getContext()
+				.getSystemService(Context.LAYOUT_INFLATER_SERVICE))
+			.inflate(R.layout.imagepref,null);
 	}
 }
