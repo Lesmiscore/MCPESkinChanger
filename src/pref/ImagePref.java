1c64f673bc6ac58e29c4b440e668bf47571209e7
nao20010128@gmail.com <nao20010128@gmail.com>
2015-08-19 12:05:35 +0900

    追加(追加予定の機能用)

---
+++ src/pref/ImagePref.java
@@ -0,0 +1,17 @@
+package pref;
+import android.util.*;
+import android.content.*;
+import android.view.*;
+
+public class ImagePref extends StartPref
+{
+	public ImagePref(Context c,AttributeSet attrs){
+		super(c,attrs);
+	}
+
+	@Override
+	public View getView(View convertView, ViewGroup parent) {
+		// TODO: Implement this method
+		return super.getView(convertView, parent);
+	}
+}
--- src/pref/StartPref.java
+++ src/pref/StartPref.java
@@ -2,16 +2,9 @@ package pref;
 import com.nao20010128nao.ToolBox.*;
 import android.util.*;
 import android.content.*;
-import android.view.*;
 
 public class StartPref extends HandledPreference{
 	public StartPref(Context c,AttributeSet attrs){
 		super(c,attrs);
-	}
-
-	@Override
-	public View getView(View convertView, ViewGroup parent) {
-		// TODO: Implement this method
-		return super.getView(convertView, parent);
 	}
 }
