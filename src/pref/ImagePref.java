a999c86f2fbf698f571ed1aeb38c0977b8e8375f
nao20010128@gmail.com <nao20010128@gmail.com>
2015-08-20 17:53:48 +0900

    いらなかった(´・ω・｀)

--- src/pref/ImagePref.java
+++ src/pref/ImagePref.java
@@ -10,12 +10,28 @@ public class ImagePref extends StartPref
 		super(c,attrs);
 		setLayoutResource(R.layout.imagepref);
 	}
-
+/*
 	@Override
 	public View getView(View convertView, ViewGroup parent) {
 		// TODO: Implement this method
-		return ((LayoutInflater)getContext()
-				.getSystemService(Context.LAYOUT_INFLATER_SERVICE))
-			.inflate(R.layout.imagepref,null);
+		convertView=((LayoutInflater)getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE)).inflate(R.layout.imagepref,null);
+		convertView.findViewById(android.R.id.icon).setId(getIdWithPackage("com.android.internal","icon",android.R.id.icon));
+		convertView.findViewById(android.R.id.title).setId(getIdWithPackage("com.android.internal","title",android.R.id.title));
+		convertView.findViewById(android.R.id.summary).setId(getIdWithPackage("com.android.internal","summary",android.R.id.summary));
+		return convertView;
 	}
+	private int getIdWithPackage(String pack,String name,int def){
+		try {
+			return Class.forName(pack + ".R$id").getField(name).getInt(null);
+		} catch (ClassNotFoundException e) {
+			
+		} catch (NoSuchFieldException e) {
+			
+		} catch (IllegalAccessException e) {
+			
+		} catch (IllegalArgumentException e) {
+			
+		}
+		return def;
+	}*/
 }
