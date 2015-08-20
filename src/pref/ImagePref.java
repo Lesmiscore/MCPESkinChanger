80ebca3003d8192babeb4e0c1e5f081a164bebed
nao20010128@gmail.com <nao20010128@gmail.com>
2015-08-21 00:25:26 +0900

    改良

--- src/pref/ImagePref.java
+++ src/pref/ImagePref.java
@@ -9,20 +9,39 @@ import java.io.*;
 import android.os.*;
 import android.app.*;
 import android.graphics.drawable.*;
+import android.widget.*;
 
 public class ImagePref extends StartPref implements ImageHandler.ImageHandlerReceiver{
 	Handler h=new Handler();
+	View v;
 	public ImagePref(Context c,AttributeSet attrs){
 		super(c,attrs);
 	}
 
 	@Override
+	public View getView(View convertView, ViewGroup parent) {
+		// TODO: Implement this method
+		return fixImage(v=super.getView(convertView, parent));
+	}
+
+	View fixImage(View v){
+		ImageView iv=(ImageView)v.findViewById(android.R.id.icon);
+		iv.setScaleType(ImageView.ScaleType.FIT_CENTER);
+		return v;
+	}
+	
+	@Override
 	public void setImage(Bitmap bmp) {
 		// TODO: Implement this method
+		bmp=Bitmap.createScaledBitmap(bmp,bmp.getWidth()*30,bmp.getHeight()*30,true);
 		final Drawable draw=new BitmapDrawable(bmp);
 		Runnable r=new Runnable(){
 			public void run(){
 				setIcon(draw);
+				if(v==null)
+					return;
+				ImageView iv=(ImageView)v.findViewById(android.R.id.icon);
+				iv.setScaleType(ImageView.ScaleType.FIT_CENTER);
 			}
 		};
 		Context ctx=getContext();
