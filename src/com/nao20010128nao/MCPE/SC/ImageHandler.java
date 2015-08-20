767ffe0d16b675eafdbd0ffaafe3aa47d38f224d
nao20010128@gmail.com <nao20010128@gmail.com>
2015-08-20 14:58:20 +0900

    ファイルのフォーマット

--- src/com/nao20010128nao/MCPE/SC/ImageHandler.java
+++ src/com/nao20010128nao/MCPE/SC/ImageHandler.java
@@ -3,42 +3,41 @@ import android.graphics.*;
 import java.util.*;
 import java.io.*;
 
-public class ImageHandler
-{
+public class ImageHandler {
 	static Map<String,HashSet<ImageHandlerReceiver>> objects=new HashMap<String,HashSet<ImageHandlerReceiver>>();
-	public static void registerReceiver(String tag,ImageHandlerReceiver receiver){
-		if(tag==null)
+	public static void registerReceiver(String tag, ImageHandlerReceiver receiver) {
+		if (tag == null)
 			throw new NullPointerException("tag == null");
-		if(receiver==null)
+		if (receiver == null)
 			throw new NullPointerException("receiver == null");
-		if(!objects.containsKey(tag)){
-			objects.put(tag,new HashSet());
+		if (!objects.containsKey(tag)) {
+			objects.put(tag, new HashSet());
 		}
 		objects.get(tag).add(receiver);
 	}
-	public static void unregisterReceiver(String tag,ImageHandlerReceiver receiver){
-		if(tag==null)
+	public static void unregisterReceiver(String tag, ImageHandlerReceiver receiver) {
+		if (tag == null)
 			throw new NullPointerException("tag == null");
-		if(receiver==null)
+		if (receiver == null)
 			throw new NullPointerException("receiver == null");
-		if(!objects.containsKey(tag))
+		if (!objects.containsKey(tag))
 			return;
 		objects.get(tag).remove(receiver);
 	}
-	public static void observe(String tag,Bitmap bmp){
-		if(tag==null)
+	public static void observe(String tag, Bitmap bmp) {
+		if (tag == null)
 			throw new NullPointerException("tag == null");
-		if(!objects.containsKey(tag))
+		if (!objects.containsKey(tag))
 			return;
-		for(ImageHandlerReceiver ihr:objects.get(tag)){
+		for (ImageHandlerReceiver ihr:objects.get(tag)) {
 			try {
 				ihr.setImage(bmp);
 			} catch (Throwable e) {
-				
+
 			}
 		}
 	}
-	public static interface ImageHandlerReceiver{
+	public static interface ImageHandlerReceiver {
 		void setImage(Bitmap bmp) throws IOException;
 	}
 }
