6faf6628ea21ed7db77052a07930fa78e63363df
nao20010128@gmail.com <nao20010128@gmail.com>
2015-08-20 19:51:01 +0900

    コンストラクタ追加

--- src/com/nao20010128nao/MCPE/SC/ImageHandler.java
+++ src/com/nao20010128nao/MCPE/SC/ImageHandler.java
@@ -4,6 +4,9 @@ import java.util.*;
 import java.io.*;
 
 public class ImageHandler {
+	private ImageHandler(){
+		
+	}
 	static Map<String,HashSet<ImageHandlerReceiver>> objects=new HashMap<String,HashSet<ImageHandlerReceiver>>();
 	public static void registerReceiver(String tag, ImageHandlerReceiver receiver) {
 		if (tag == null)
