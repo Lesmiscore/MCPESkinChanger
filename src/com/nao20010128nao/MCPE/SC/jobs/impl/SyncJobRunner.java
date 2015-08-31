703ebd380bc03c1b2c80e33a67f0973b88ef08e8
nao20010128@gmail.com <nao20010128@gmail.com>
2015-08-31 16:55:10 +0900

    impl追加(中身は空っぽ)

---
+++ src/com/nao20010128nao/MCPE/SC/jobs/impl/SyncJobRunner.java
@@ -0,0 +1,88 @@
+package com.nao20010128nao.MCPE.SC.jobs.impl;
+import com.nao20010128nao.MCPE.SC.jobs.*;
+import com.nao20010128nao.MCPE.SC.jobs.JobRunner.*;
+import java.util.*;
+
+public class SyncJobRunner extends JobRunner{
+	@Override
+	public void clearAllDataHandler() {
+		// TODO: Implement this method
+	}
+	@Override
+	public void clearAllResultHandler() {
+		// TODO: Implement this method
+	}
+	@Override
+	public void removeDataHandler(JobRunner.JobDataHandler handler) {
+		// TODO: Implement this method
+	}
+	@Override
+	public void removeResultHandler(JobRunner.JobResultHandler handler) {
+		// TODO: Implement this method
+	}
+	@Override
+	public void addResultHandlers(JobRunner.JobResultHandler[] handler) {
+		// TODO: Implement this method
+	}
+	@Override
+	public void addDataHandlers(JobRunner.JobDataHandler[] handler) {
+		// TODO: Implement this method
+	}
+	@Override
+	public void addDataHandlers(List<JobRunner.JobDataHandler> handler) {
+		// TODO: Implement this method
+	}
+	@Override
+	public void setJobs(Job[] jobs) {
+		// TODO: Implement this method
+	}
+
+	@Override
+	public void setAsync(boolean value) {
+		// TODO: Implement this method
+	}
+	@Override
+	public boolean isAsync() {
+		// TODO: Implement this method
+		return false;
+	}
+	@Override
+	public JobRunner.JobDataHandler[] getDataHandlers() {
+		// TODO: Implement this method
+		return null;
+	}
+	@Override
+	public JobRunner.JobResultHandler[] getResultHandlers() {
+		// TODO: Implement this method
+		return null;
+	}
+	@Override
+	public void stopJobs() {
+		// TODO: Implement this method
+	}
+	@Override
+	public void addResultHandler(JobRunner.JobResultHandler handler) {
+		// TODO: Implement this method
+	}
+	@Override
+	public void addResultHandlers(List<JobRunner.JobResultHandler> handler) {
+		// TODO: Implement this method
+	}
+	@Override
+	public boolean canStopJobs() {
+		// TODO: Implement this method
+		return false;
+	}
+	@Override
+	public void addDataHandler(JobRunner.JobDataHandler handler) {
+		// TODO: Implement this method
+	}
+	@Override
+	public void restartJobs() {
+		// TODO: Implement this method
+	}
+	@Override
+	public void runJobs() {
+		// TODO: Implement this method
+	}
+}
