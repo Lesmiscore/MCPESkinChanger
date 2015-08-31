8d157f9ecbb9f8c29988302ad89b2a31ea4d3170
nao20010128@gmail.com <nao20010128@gmail.com>
2015-08-31 20:53:49 +0900

    ファイル署名Activity作成

---
+++ src/com/nao20010128nao/MCPE/SC/SignFileActivity.java
@@ -0,0 +1,11 @@
+package com.nao20010128nao.MCPE.SC;
+import android.app.*;
+import android.os.*;
+
+public class SignFileActivity extends Activity{
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO: Implement this method
+		super.onCreate(savedInstanceState);
+	}
+}
