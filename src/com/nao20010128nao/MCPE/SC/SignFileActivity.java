eb08c93de02665653dc4b2f1e519bccf106dd008
nao20010128@gmail.com <nao20010128@gmail.com>
2015-08-31 21:22:13 +0900

    いろいろ(署名系)

--- src/com/nao20010128nao/MCPE/SC/SignFileActivity.java
+++ src/com/nao20010128nao/MCPE/SC/SignFileActivity.java
@@ -1,11 +1,13 @@
 package com.nao20010128nao.MCPE.SC;
 import android.app.*;
 import android.os.*;
+import com.nao20010128nao.MC_PE.SkinChanger.*;
 
 public class SignFileActivity extends Activity{
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO: Implement this method
 		super.onCreate(savedInstanceState);
+		setContentView(R.layout.signer);
 	}
 }
