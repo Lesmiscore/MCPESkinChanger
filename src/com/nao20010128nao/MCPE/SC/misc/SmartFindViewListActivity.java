34eda4413bb260715870bbaf4aed01e7f05c3a20
nao20010128@gmail.com <nao20010128@gmail.com>
2015-09-10 22:51:20 +0900

    いろいろ

---
+++ src/com/nao20010128nao/MCPE/SC/misc/SmartFindViewListActivity.java
@@ -0,0 +1,48 @@
+package com.nao20010128nao.MCPE.SC.misc;
+import android.app.*;
+import android.view.*;
+
+public abstract class SmartFindViewListActivity extends ListActivity {
+	@Override
+	public <T extends View> T find(int id) {
+		// TODO: Implement this method
+		return (T)findViewById(id);
+	}
+	@Override
+	public <T extends View> T find(String id) {
+		// TODO: Implement this method
+		try {
+			return find((int)Class.forName(getPackageName() + ".R$id").getField(id).get(null));
+		} catch (ClassNotFoundException e) {
+			
+		} catch (NoSuchFieldException e) {
+			
+		} catch (IllegalAccessException e) {
+			
+		} catch (IllegalArgumentException e) {
+			
+		}
+		return null;
+	}
+	@Override
+	public <T extends View> T find(int id,View parent) {
+		// TODO: Implement this method
+		return (T)parent.findViewById(id);
+	}
+	@Override
+	public <T extends View> T find(String id,View parent) {
+		// TODO: Implement this method
+		try {
+			return find((int)Class.forName(getPackageName() + ".R$id").getField(id).get(null),parent);
+		} catch (ClassNotFoundException e) {
+
+		} catch (NoSuchFieldException e) {
+
+		} catch (IllegalAccessException e) {
+
+		} catch (IllegalArgumentException e) {
+
+		}
+		return null;
+	}
+}
--- src/com/nao20010128nao/MCPE/SC/plugin/PluginLauncher.java
+++ src/com/nao20010128nao/MCPE/SC/plugin/PluginLauncher.java
@@ -1,5 +1,6 @@
 package com.nao20010128nao.MCPE.SC.plugin;
+import com.nao20010128nao.MCPE.SC.misc.*;
 
-public class PluginLauncher
+public class PluginLauncher extends SmartFindViewActivity
 {
 }
