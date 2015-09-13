a655b572a6246148d4e9dde1c5602cd27fbbf79c
nao20010128nao <nao20010128@gmail.com>
2015-09-13 11:33:04 +0900

    ついか

---
+++ src/com/nao20010128nao/MCPE/SC/plugin/DiffMap.java
@@ -0,0 +1,66 @@
+package com.nao20010128nao.MCPE.SC.plugin;
+import java.util.*;
+import java.util.Map.*;
+
+public class DiffMap<K,V> implements Map<K,V> {
+	Map<K,V> base,diff;
+	Set<K> remove=new HashSet<>();
+	@Override
+	public V put(K p1, V p2) {
+		// TODO: Implement this method
+		return null;
+	}
+	@Override
+	public void putAll(Map<? extends K, ? extends V> p1) {
+		// TODO: Implement this method
+	}
+	@Override
+	public Set<Map.Entry<K, V>> entrySet() {
+		// TODO: Implement this method
+		return null;
+	}
+	@Override
+	public boolean containsKey(Object p1) {
+		// TODO: Implement this method
+		return false;
+	}
+	@Override
+	public boolean containsValue(Object p1) {
+		// TODO: Implement this method
+		return false;
+	}
+	@Override
+	public V get(Object p1) {
+		// TODO: Implement this method
+		return null;
+	}
+	@Override
+	public int size() {
+		// TODO: Implement this method
+		return 0;
+	}
+	@Override
+	public V remove(Object p1) {
+		// TODO: Implement this method
+		return null;
+	}
+	@Override
+	public Collection<V> values() {
+		// TODO: Implement this method
+		return null;
+	}
+	@Override
+	public Set<K> keySet() {
+		// TODO: Implement this method
+		return null;
+	}
+	@Override
+	public boolean isEmpty() {
+		// TODO: Implement this method
+		return false;
+	}
+	@Override
+	public void clear() {
+		// TODO: Implement this method
+	}
+}
