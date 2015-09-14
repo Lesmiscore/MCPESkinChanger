13e25f0ca94ac6ddba178fe332ab6e4b15b0d6d6
nao20010128nao <nao20010128@gmail.com>
2015-09-15 00:46:02 +0900

    いろいろ

--- src/com/nao20010128nao/MCPE/SC/plugin/PluginLauncher.java
+++ src/com/nao20010128nao/MCPE/SC/plugin/PluginLauncher.java
@@ -7,6 +7,8 @@ import java.util.*;
 import android.app.*;
 import android.content.*;
 import android.view.*;
+import java.io.*;
+import com.nao20010128nao.SpoofBrowser.classes.*;
 
 public class PluginLauncher extends SmartFindViewListActivity
 {
@@ -61,5 +63,39 @@ public class PluginLauncher extends SmartFindViewListActivity
 				}
 				break;
 		}
+	}
+	DiffMap<String,Byte[]> deserializeDiff(Map<String,Byte[]> base,byte[] data){
+		try {
+			DataInputStream dis=new DataInputStream(new ByteArrayInputStream(data));
+			Set<String> removes=new HashSet<>();
+			Map<String,Byte[]> additions=new HashMap<>();
+			int remSize=dis.readInt();
+			for (int i=0;i < remSize;i++) {
+				removes.add(dis.readUTF());
+			}
+			int addSize=dis.readInt();
+			for (int i=0;i < addSize;i++) {
+				String k=dis.readUTF();
+				byte[] v=new byte[dis.readShort()];
+				dis.readFully(v);
+				additions.put(k, primToWrap(v));
+			}
+			DiffMap<String,Byte[]> result=new DiffMap<>(base);
+			for (String s:removes)
+				result.remove(s);
+			for (Map.Entry<String,Byte[]> entry:additions.entrySet())
+				result.put(entry.getKey(), entry.getValue());
+			return result;
+		} catch (IOException e) {
+			
+		}
+		return new DiffMap<>(Collections.emptyMap());
+	}
+	Byte[] primToWrap(byte[] prim){
+		Byte[] wrap=new Byte[prim.length];
+		for(int i=0;i<prim.length;i++){
+			wrap[i]=prim[i];
+		}
+		return wrap;
 	}
 }
