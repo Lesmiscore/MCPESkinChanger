99a141247037ac3b3699f242482ed1d24b5507bd
nao20010128nao <nao20010128@gmail.com>
2015-09-23 15:44:04 +0900

    いろいろ

--- src/com/nao20010128nao/MCPE/SC/AppInfoActivity.java
+++ src/com/nao20010128nao/MCPE/SC/AppInfoActivity.java
@@ -1,6 +1,14 @@
 package com.nao20010128nao.MCPE.SC;
 import android.app.*;
+import android.widget.*;
+import java.io.*;
+import android.os.*;
 
 public class AppInfoActivity extends Activity
 {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO: Implement this method
+		super.onCreate(savedInstanceState);
+	}
 }
