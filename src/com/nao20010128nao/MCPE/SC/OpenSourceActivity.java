99eaa5c9c93b370e6d0c502184e3a612ec25b7a5
nao20010128nao <nao20010128@gmail.com>
2015-10-04 10:41:51 +0900

    オープンソースライセンスActivityの実装

--- src/com/nao20010128nao/MCPE/SC/OpenSourceActivity.java
+++ src/com/nao20010128nao/MCPE/SC/OpenSourceActivity.java
@@ -1,6 +1,18 @@
 package com.nao20010128nao.MCPE.SC;
 import android.app.*;
+import android.os.*;
+import android.webkit.*;
+import com.nao20010128nao.MCPE.SC.misc.*;
+import com.nao20010128nao.MC_PE.SkinChanger.*;
 
-public class OpenSourceActivity extends Activity
+public class OpenSourceActivity extends SmartFindViewActivity
 {
+	WebView view;
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO: Implement this method
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.license);
+		view=find("license");
+	}
 }
