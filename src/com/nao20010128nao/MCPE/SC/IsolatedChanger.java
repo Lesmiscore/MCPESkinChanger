51f2d1c4b0312bd701628824f36b36d3a1f4a9a2
nao20010128nao <nao20010128@gmail.com>
2015-10-18 10:03:36 +0900

    独立プロセス用

---
+++ src/com/nao20010128nao/MCPE/SC/IsolatedChanger.java
@@ -0,0 +1,11 @@
+package com.nao20010128nao.MCPE.SC;
+import java.util.*;
+import java.net.*;
+
+public class IsolatedChanger
+{
+	Map<String,URI> converts=new FilteredMap();
+	public static void main(String[] args){
+		
+	}
+}
