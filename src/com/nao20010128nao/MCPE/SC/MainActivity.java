77f0ba7df7619f428724869967e7313aeb8505ef
nao20010128nao <nao20010128@gmail.com>
2015-10-31 17:14:45 +0900

    軽量化

--- src/com/nao20010128nao/MCPE/SC/MainActivity.java
+++ src/com/nao20010128nao/MCPE/SC/MainActivity.java
@@ -1,16 +1,1 @@
-package com.nao20010128nao.MCPE.SC;
-import android.app.*;
-import android.os.*;
-import android.content.*;
-
-public class MainActivity extends Activity
-{
-
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		// TODO: Implement this method
-		super.onCreate(savedInstanceState);
-		startActivity(new Intent(this,SplashActivity.class));
-		finish();
-	}
-}
+package com.nao20010128nao.MCPE.SC;public class MainActivity extends SplashActivity{}
