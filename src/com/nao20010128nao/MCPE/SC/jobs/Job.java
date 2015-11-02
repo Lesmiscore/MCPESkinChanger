d39c9472c1c45b95f11eef0cf5d9640fc1ff076a
nao20010128nao <nao20010128@gmail.com>
2015-11-02 22:20:23 +0900

    削除

--- src/com/nao20010128nao/MCPE/SC/jobs/Job.java
+++
@@ -1,9 +0,0 @@
-package com.nao20010128nao.MCPE.SC.jobs;
-
-public interface Job{
-	/**
-	@param jobData A JobArgument.
-	@return is the job finished completely?
-	*/
-	public boolean run(JobArgument jobData);
-}
