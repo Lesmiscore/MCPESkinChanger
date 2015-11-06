2be061f3616a8fd4c162aabc09a6f4bb9385f53e
nao20010128nao <nao20010128@gmail.com>
2015-11-06 18:06:46 +0900

    削除

--- src/com/nao20010128nao/MCPE/SC/RunOnceApplication.java
+++ src/com/nao20010128nao/MCPE/SC/RunOnceApplication.java
@@ -51,45 +51,6 @@ public class RunOnceApplication extends Application {
 	public UUIDs getUuids() {
 		return uuids;
 	}
-	public void loadMcpeApk(){
-		InputStream is=null;
-		OutputStream os=null;
-		try {
-			os=new FileOutputStream(new File(getExternalCacheDir(),"mcpe.apk"));
-			is = openAPK();
-			byte[]buf=new byte[10000];
-			while (true) {
-				int r=is.read(buf);
-				if(r<=0){
-					break;
-				}
-				os.write(buf,0,r);
-			}
-		} catch (Throwable e) {
-
-		}finally{
-			try {
-				is.close();
-			} catch (Throwable e) {
-
-			}
-			try {
-				os.close();
-			} catch (Throwable e) {
-
-			}
-		}
-	}
-	public InputStream openAPK() throws IOException,PackageManager.NameNotFoundException {
-		switch (Tools.getSettings("input.mode", 0, this)) {
-			case 0://installed
-				return new FileInputStream(createPackageContext("com.mojang.minecraftpe", CONTEXT_IGNORE_SECURITY).getPackageCodePath());
-			case 1://select
-				return tryOpen(Tools.getSettings("input.where", "", this));
-			default:
-				return null;
-		}
-	}
 	public String getApkPath() {
 		switch (Tools.getSettings("input.mode", 0, this)) {
 			case 0://installed
@@ -102,16 +63,6 @@ public class RunOnceApplication extends Application {
 				return Tools.getSettings("input.where", "", this);
 			default:
 				return null;
-		}
-	}
-	public InputStream tryOpen(String uri) throws IOException {
-		Log.d("dbg", "tryOpen:" + uri);
-		if (uri.startsWith("content://")) {
-			return getContentResolver().openInputStream(Uri.parse(uri));
-		} else if (uri.startsWith("/")) {
-			return new FileInputStream(uri);
-		} else {
-			return URI.create(uri).toURL().openConnection().getInputStream();
 		}
 	}
 }
